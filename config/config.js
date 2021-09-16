d3b4e281a3f9a987bfade7e7ba642dc78c0acfaf
jkojjcn <juancojjcn@gmail.com>
2021-09-16 13:48:22 -0500

    mikunav2

--- config/config.js
+++ config/config.js
@@ -11,15 +11,27 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 
-const databaseConfig = {
-    'host': 'ec2-54-146-84-101.compute-1.amazonaws.com',
+/*const databaseConfig = {
+    'host': 'ec2-54-146-84-101.compute-1.amazonaws.com/',
     'port': 5432,
     'database': 'dfp23mevs3oe30',
     'user' : 'vtkdjenwmtvbpi',
     'password': '09d15179e8b1bd2dac178feb0bdcdc40a6ac65ecf5f39e3e27c710dc31a57f4e'
-};
+};*/
+let cloud_config = {
+    username: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    port: 5432,
+    ssl: true,
+    dialect: 'postgres',
+    dialectOptions: {
+      "ssl": {"require":true }
+    }
+  };
 
 
-const db = pgp(databaseConfig);
+const db = pgp(cloud_config);
 
 module.exports = db;
