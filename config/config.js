bc99a018ac73d0f09b13fbaaf58e9c4dc1562d29
jkojjcn <juancojjcn@gmail.com>
2021-09-16 13:05:36 -0500

    newMikuna

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 const databaseConfig = {
-    'host': 'ec2-54-146-84-101.compute-1.amazonaws.com',
+    'host': 'postgres://vtkdjenwmtvbpi:09d15179e8b1bd2dac178feb0bdcdc40a6ac65ecf5f39e3e27c710dc31a57f4e@ec2-54-146-84-101.compute-1.amazonaws.com:5432/dfp23mevs3oe30',
     'port': 5432,
     'database': 'dfp23mevs3oe30',
     'user' : 'vtkdjenwmtvbpi',
