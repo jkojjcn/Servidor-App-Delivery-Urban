e81e7264d897b78010ab914cb52921734a3f63a1
jkojjcn <juancojjcn@gmail.com>
2022-01-10 06:57:04 -0500

    allOrdersDelivery

--- models/order.js
+++ models/order.js
@@ -451,7 +451,7 @@ Order.updateAcepted = (order) => {
         acepted = 'Aceptado',
         id_delivery = $2
     WHERE
-        id = $1 AND acepted = NULL
+        id = $1 AND id_delivery = NULL
     `;
     return db.none(sql, [
         order.id,
