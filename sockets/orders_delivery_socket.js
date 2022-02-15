177c7a109c07cda393326cbe0dc7402c66fd3293
JuanCho <juancojjcn@gmail.com>
2022-02-15 01:00:16 -0500

    newEmit

--- sockets/orders_delivery_socket.js
+++ sockets/orders_delivery_socket.js
@@ -25,7 +25,7 @@ module.exports = (io) => {
 
         socket.on('positionAD', function(data) {
             console.log(`EMITIO ${JSON.stringify(data)}`);
-            orderDeliveryNamespace.emit(`position/aD`, { lat: data.lat, lng: data.lng  });
+            allDeliveryLocation.emit(`position/${data.id_rest}`, { lat: data.lat, lng: data.lng  });
         });
 
         socket.on('disconnect', function(data) {
