1f70c9329d91bc92447c091f2765248e7c1f8771
JuanCho <juancojjcn@gmail.com>
2022-02-27 03:34:17 -0500

    addReceiver

--- models/message.js
+++ models/message.js
@@ -6,18 +6,37 @@ Message.findMessage = (id_user) => {
     const sql = `
     SELECT
         id,
-        email,
-        name,
-        lastname,
-        phone,
-        image,
+        to,
         open,
         message,
         created_at,
         updated_at,
-        notification_token
+        JSON_BUILD_OBJECT(
+            'id', U.id,
+            'name', U.name,
+            'lastname', U.lastname,
+            'image', U.image,
+            'notification_token', U.notification_token,
+            'phone', U.phone
+        ) AS client,
+		JSON_BUILD_OBJECT(
+            'id', U2.id,
+            'name', U2.name,
+            'lastname', U2.lastname,
+            'image', U2.image,
+            'phone', U2.phone,
+            'notification_token', U2.notification_token
+        ) AS to,
     FROM
         message
+    INNER JOIN
+        users AS U
+    ON
+        id = U.id
+	LEFT JOIN
+		users AS U2
+	ON
+		to = U2.id
     WHERE
         id = $1
     `;
--- routes/messajeRoutes.js
+++ routes/messajeRoutes.js
@@ -2,8 +2,6 @@ const MessageController = require('../controllers/messageController');
 const passport = require('passport');
 
 module.exports = (app, upload) => {
-
-    app.get('/api/message/findMessage/:id_user', MessageController.findById);
-
+    app.get('/api/message/findMessage/:id_user', passport.authenticate('jwt', {session: false}), MessageController.findById);
    // app.post('/api/restaurants/create', passport.authenticate('jwt', {session: false}), upload.array('image', 3), ProductsController.create);
 }
