391b36747f092b6744c23401bf73f86b4b9088b8
JuanCho <juancojjcn@gmail.com>
2022-04-18 13:18:06 -0500

    messageAdd

--- models/message.js
+++ models/message.js
@@ -58,7 +58,7 @@ Message.create = (message) => {
         created_at,
         updated_at,
         )
-    VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id
+    VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING from
     `;
 
     return db.oneOrNone(sql, [
