ab647bbfa4425d91d4cbd3551c407c160a29bc27
JuanCho <juancojjcn@gmail.com>
2022-07-17 14:26:26 -0500

    newBackendTaxiServer

--- controllers/taxiController.js
+++ controllers/taxiController.js
@@ -43,6 +43,29 @@ module.exports = {
                 error: error
             });
         }
-    }
+    },
+
+    async getAllRequest(req, res, next) {
+
+        try {
+            const id_user = req.params.id_user;
+            if(id_user = 1){
+                const data = await TaxiController.getAllRequest(id_user);
+            console.log(`Solicitud ${JSON.stringify(data)}`);
+            return res.status(201).json(data);
+
+            }
+            
+        } 
+        catch (error) {
+            console.log(`Error ${error}`);    
+            return res.status(501).json({
+                message: 'Hubo un error al tratar de obtener las solicitudes',
+                error: error,
+                success: false
+            })
+        }
+
+    },
 
 }
--- routes/taxiRoutes.js
+++ routes/taxiRoutes.js
@@ -5,6 +5,8 @@ module.exports = (app) => {
 
    app.get('/api/taxi/findByUser/:id_user', passport.authenticate('jwt', {session: false}), TaxiController.findByUser);
 
+   app.get('/api/taxi/getAllRequest/:id_user', passport.authenticate('jwt', {session: false}), TaxiController.getAllRequest);
+
 
    app.post('/api/taxi/create', passport.authenticate('jwt', {session: false}), TaxiController.create);
 }
