0ca9373ddc14549b0bd35ba938228113b99ab23c
JuanCho <juancojjcn@gmail.com>
2022-10-14 21:44:46 -0500

    newServerRush

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 module.exports = (app, upload) => {
 
     // TRAER DATOS
-    app.get('/api/users/getAll/:id', UsersController.getAll);
+    app.get('/api/users/getAll/:id', passport.authenticate('jwt', {session: false}), UsersController.getAll);
     app.get('/api/users/findById/:id', passport.authenticate('jwt', {session: false}), UsersController.findById);
     app.get('/api/users/findDeliveryMen', passport.authenticate('jwt', {session: false}), UsersController.findDeliveryMen);
     app.get('/api/users/getAdminsNotificationTokens', passport.authenticate('jwt', {session: false}), UsersController.getAdminsNotificationTokens);
@@ -15,7 +15,7 @@ module.exports = (app, upload) => {
     app.post('/api/users/logout', UsersController.logout);
 
     // ACTUALIZAR DATOS
-    app.put('/api/users/update', UsersController.update);
-    app.put('/api/users/updateWithImage', upload.array('image', 1), UsersController.updateWithImage);
+    app.put('/api/users/update', passport.authenticate('jwt', {session: false}), UsersController.update);
+    app.put('/api/users/updateWithImage', passport.authenticate('jwt', {session: false}), upload.array('image', 1), UsersController.updateWithImage);
     app.put('/api/users/updateNotificationToken', UsersController.updateNotificationToken);
 }
