4253c5cfae3cd2ae669e3e92a8d25c40672a187b
JuanCho <juancojjcn@gmail.com>
2022-10-15 17:38:49 -0500

    newServerRush

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -7,5 +7,5 @@ module.exports = (app, upload) => {
     app.post('/api/message/create', passport.authenticate('jwt', {session: false}), MessageController.create);
     app.post('/api/message/createWithImage', passport.authenticate('jwt', {session: false}), upload.array('image', 1), MessageController.createWithImage);
 
-
+// testUpload
 }
