541c0802fac9f0a67d0b0e97ae4eee6f90d6d2d6
JuanCho <juancojjcn@gmail.com>
2022-10-15 09:48:24 -0500

    newServerRush

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,5 +2,5 @@ const MessageController = require('../controllers/messageController');
 const passport = require('passport');
 
 module.exports = (app, upload) => {
-    app.get('/api/message/create', passport.authenticate('jwt', {session: false}), MessageController.create);
+    app.post('/api/message/create', passport.authenticate('jwt', {session: false}), MessageController.create);
 }
