38566375cf554aea2d84b7d135604d0d3c0c02a1
JuanCho <juancojjcn@gmail.com>
2022-10-15 16:38:18 -0500

    newServerRush

--- sockets/chat_socket.js
+++ sockets/chat_socket.js
@@ -6,7 +6,10 @@ module.exports = (io) => {
     const chatNSP = io.of('/chat');
     chatNSP.on('connection', function(socket){
         console.log('USUARIO SE CONECTÓ A SOCKET IO CHAT', socket.id);
-
+        socket.on('message', function(data){
+            console.log('Nuevo Mensaje', data);
+            chatNSP.emit(`mensage/${data.id_chat}`, data);
+        });
         socket.on('disconnect', function(data){
             console.log('UN USUARIO SE DESCONECTÓ DE CHAT', socket.id);
         });
@@ -14,5 +17,7 @@ module.exports = (io) => {
 
 
     })
+
+    // ``
 
 }
