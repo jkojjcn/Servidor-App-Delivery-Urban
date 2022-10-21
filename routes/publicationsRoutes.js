641d40ca82e046e2c208f703d24c726afae79a89
JuanCho <juancojjcn@gmail.com>
2022-10-20 19:25:09 -0500

    newServerRush

--- routes/publicationsRoutes.js
+++ routes/publicationsRoutes.js
@@ -1,10 +1,9 @@
 const PublicationsController = require('../controllers/publicationsController');
-const passport = require('passport');
 
 module.exports = (app) => {
 
    
-    app.get('/api/publications/getAll/', PublicationsController.getAll);
+    app.get('/api/publications/getAll', PublicationsController.getAll);
 
 
 
